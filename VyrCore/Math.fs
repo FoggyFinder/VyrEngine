47735d09fbf29790ae0f839c1d224f9469ab0507
Phil <Phil@PHIL-PC>
2016-09-28 22:17:23 +0200

    - Added math functions for vec2

--- VyrCore/Math.fs
+++ VyrCore/Math.fs
@@ -1,16 +1,23 @@
 ﻿namespace VyrCore
 
-open MathNet.Numerics
-
 module Math =
+    let inline approximately x y = abs (x - y) < System.Double.Epsilon
     /// Checks if a generic numerical is a power of 2
     let inline isPowerOf2 x = (x &&& (x - LanguagePrimitives.GenericOne)) = LanguagePrimitives.GenericZero
 
 module MathVec2 =
-    /// Calculates the squared length of a Vec2
+    /// Calculates the squared length of a vector
     let inline squaredMagnitude (v:Vec2<_>) = let genTwo = genericTwo() in (v.X ** genTwo) + (v.Y ** genTwo)
-    /// Calculates the length of a Vec2
+    /// Calculates the length of a vector
     let inline magnitude (v:Vec2<_>) = v |> squaredMagnitude |> sqrt
+    /// Calculates the dot product of two vectors
+    let inline dot (v1:Vec2<_>) (v2:Vec2<_>) = v1.X * v2.X + v1.Y * v2.Y
+    /// Calculates the angle of two vectors by using the acos of the dot product divided by their magnitudes
+    let inline angle (v1:Vec2<_>) (v2:Vec2<_>) = (dot v1 v2) / ((magnitude v1) * (magnitude v2)) |> acos
+    /// Checks if 2 vectors are orthogonal by calculating their dot product and testing against zero
+    let inline isOrthogonal (v1:Vec2<_>) (v2:Vec2<_>) = (dot v1 v2) |> Math.approximately LanguagePrimitives.GenericZero
+    /// Checks if 2 vectors lie on the same side of a plane
+    let inline isSameSided (v1:Vec2<_>) (v2:Vec2<_>) = ((dot v1 v2) |> sign) > LanguagePrimitives.GenericZero
 
 module MathVec3 =
     /// Calculates the squared length of a Vec2
@@ -25,4 +32,5 @@ module MathVec4 =
     let inline magnitude (v:Vec4<_>) = v |> squaredMagnitude |> sqrt   
 
 //module Trigonometry =
-   // let inline triangleInequality (v:Vec)
+    /// Checks if two vectors can form a triangle with a 
+    //let inline isTriangle v1 v2 v3
---
+++ VyrCore/TextMath.fsx
@@ -0,0 +1,15 @@
+﻿#load "FSharpTypes.fs"
+#load "Basic.fs"
+#load "Math.fs"
+
+open VyrCore
+
+let v1 = Vec2(0., 1.)
+let v2 = Vec2(1., 0.)
+let dot = MathVec2.dot v1 v2
+let orthogonal = MathVec2.isOrthogonal v1 v2
+
+let v3 = Vec2(0.5, -1.)
+let v4 = Vec2(1., 0.5)
+let dot2 = MathVec2.dot v3 v4
+let orthogonal2 = MathVec2.isOrthogonal v3 v4
